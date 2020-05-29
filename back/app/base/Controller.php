4f8c8796d4519c53518c6ffe0c090d3da4794199
Rodolfo <rodolfo.dmn@gmail.com>
2020-05-29 09:24:34 -0300

    melhorias na estrutura

---
+++ back/app/base/Controller.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace back\app\base;
+
+/**
+ * Class Controller
+ * @author Rodolfo
+ */
+class Controller
+{
+    /**
+     * request function
+     *
+     * @return void
+     */
+    public function request($param)
+    {
+        return null;
+    }
+    
+}
---
+++ back/app/base/Handler.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace back/app/base;
+
+/**
+ * Class Handler
+ * @author Rodolfo
+ */
+class Handler
+{
+    /**
+     * action function
+     *
+     * @return void
+     */
+    public function action($param)
+    {
+        return null;
+    }
+    
+}
